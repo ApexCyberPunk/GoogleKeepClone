0c34a766d8535494bee23ed77623883256a09b7a
snovacain <61154743+snovacain@users.noreply.github.com>
2023-09-13 20:51:43 -0700

    update
    things learned
    
    getBoundingClientRect();
    
    window.scrollY and window.scrollX

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ class App {
     this.$modalTitle = document.querySelector(".modal-title")
     this.$modalText = document.querySelector(".modal-text")
     this.$modalCloseButton = document.querySelector('.modal-close-button')
+    this.$colorTooltip = document.querySelector('#color-tooltip')
+
 
     this.addEventListeners()
 
@@ -28,6 +30,29 @@ class App {
       this.selectNote(event)
       this.openModal(event)
     })
+
+    document.body.addEventListener('mouseover', event => {
+      this.openTooltip(event)
+    })
+    document.body.addEventListener('mouseout', event => {
+      this.closeTooltip(event)
+    })
+
+    this.$colorTooltip.addEventListener('mouseover', function() {
+      this.style.display = "flex"
+    })
+
+    this.$colorTooltip.addEventListener('mouseout', function() {
+      this.style.display = 'none'
+    })
+
+    this.$colorTooltip.addEventListener('click', event => {
+      const color = event.target.dataset.color;
+      if (color) {
+        this.editNoteColor(color);
+      }
+    })
+
 
     this.$form.addEventListener('submit', event =>
     {
@@ -107,6 +132,22 @@ this.editNote()
 this.$modal.classList.toggle('open-modal')
 }
 
+openTooltip(event) {
+  if (!event.target.matches('.toolbar-color')) return;
+  this.id = event.target.dataset.id;
+  const noteCoords = event.target.getBoundingClientRect();
+  const horizontal = noteCoords.left + window.scrollX;
+  const vertical = noteCoords.top + window.scrollY;
+  this.$colorTooltip.style.transform = `translate(${horizontal}px, ${vertical}px)`
+  this.$colorTooltip.style.display = 'flex'
+
+}
+
+closeTooltip(event) {
+  if (!event.target.matches('.toolbar-color')) return;
+  this.$colorTooltip.style.display = 'none'
+}
+
 addNote({title, text}) {
 const newNote = {
   title,
@@ -124,6 +165,13 @@ const title = this.$modalTitle.value
 const text = this.$modalText.value
 this.notes = this.notes.map(note => note.id === Number(this.id) ? {...note, title, text} : note);
 this.displayNotes();
+}
+
+editNoteColor(color) {
+  this.notes = this.notes.map(note =>
+    note.id === Number(this.id) ? {...note, color } : note
+  );
+  this.displayNotes();
 }
 
 selectNote(event) {
@@ -147,7 +195,7 @@ displayNotes() {
 <div class="note-text">${mOfNotes.text}</div>
 <div class="toolbar-container">
 <div class="toolbar">
-    <img class="toolbar-color" src="https://icon.now.sh/palette">
+    <img class="toolbar-color" data-id=${mOfNotes.id} src="https://icon.now.sh/palette">
     <img class="toolbar-delete" src="https://icon.now.sh/delete">
       </div>
     </div>
